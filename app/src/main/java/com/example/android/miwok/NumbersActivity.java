72999099d0f8bb73ab1afb44f57e035ed7b8ca52
sTsaturian <sTsaturian@users.noreply.github.com>
2020-05-20 23:51:06 -0400

    Added words to the Numbers page

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,12 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("one","two","three","four",
+                "five","six","seven","eight","nine","ten"));
+        LinearLayout rootView = findViewById(R.id.rootView);
+        for (String word: words){
+            TextView wordView = new TextView(this);
+            wordView.setText(word);
+        }
     }
 }
